a623cb838f1548fb2a6bd3e68642aafa60d07847
plitche <kysu728@naver.com>
2020-12-24 20:20:06 +0900

    2020.12.24

---
+++ src/stage03_for문/Q2742/Answer1.java
@@ -0,0 +1,24 @@
+package stage03_for문.Q2742;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Answer1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		while (N-- > 0) {
+			bw.append(String.valueOf(N+1));
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+}
