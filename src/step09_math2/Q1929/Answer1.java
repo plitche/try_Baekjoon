43418865198c47519c231d7111e1ca7c331c72e0
plitche <kysu728@naver.com>
2020-12-25 21:23:25 +0900

    2020.12.25

--- src/step09_math2/Q1929/Answer1.java
+++ src/step09_math2/Q1929/Answer1.java
@@ -23,15 +23,22 @@ public class Answer1 {
 		int N = Integer.parseInt(st.nextToken());	//끝 숫자
 		// M과 N사이의 소수들을 오름차순으로 정렬하라.
 		
-		int[] array = new int[N+1]; 
-		for (int i=M; i<=N; i++) {
-			for (int j=2; j<i/2; j++) {	//1과 본인 제외하고 나누었을때 안나누어떨어져야 소수
-				if (i%j==0) {
-					array[i] = 1;	//나누어 떨어진 index에 0이 아닌 숫자 넣기
+		boolean[] primeNo = new boolean[N+1];	//소수가 아닌 숫자들을 true로 바꾸기 
+		primeNo[1] = true;	//1은 소수가 아니다.
+		
+		for (int i=2; i<N/2; i++) {
+			if (primeNo[i]==false) {
+				for (int j=i+1; j<=N; j++) {
+					if (j%i==0) {	//2이상 i의 배수들은 전부 소수가 아니다. 
+						primeNo[j] = true;
+					}
 				}
 			}
-			if (array[i]==0) { // 1로만 나누어 떨어진 숫자: 소수
-				sb.append(i).append("\n");	//StringBuilder에 소수 하나씩 저장
+		}
+
+		for (int i=M; i<=N; i++) {
+			if (primeNo[i]==false) {
+				sb.append(i).append("\n");
 			}
 		}
 		
--- src/step09_math2/Q2581/Answer1.java
+++ src/step09_math2/Q2581/Answer1.java
@@ -11,7 +11,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Wrong1 {
+public class Answer1 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
