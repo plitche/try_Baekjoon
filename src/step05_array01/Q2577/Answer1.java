7c1525aa76d1e53238ba30fb22d680bc5e67e673
plitche <kysu728@naver.com>
2020-12-30 19:41:15 +0900

    2020.12.30

--- src/step05_array01/Q2577/Answer1.java
+++ src/step05_array01/Q2577/Answer1.java
@@ -1,5 +1,36 @@
 package step05_array01.Q2577;
 
+import java.util.Scanner;
+
 public class Answer1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int multiple = A*B*C;
+		char[] array = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+		int[] count = new int[10];
+		
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<array.length; j++) {
+				if (String.valueOf(multiple).length()>i) {
+					if (String.valueOf(multiple).charAt(i) == array[j]) {
+						count[j] += 1;
+					}
+				}
+			}
+		}
+		
+		for (int i=0; i<count.length; i++) {
+			System.out.println(count[i]);
+		}
+		
+		sc.close();
+		
+	}
 
 }
