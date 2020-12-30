97fd6097f5fe11dd61a843a8ce6d5c3be17b07c0
plitche <kysu728@naver.com>
2020-12-30 19:40:00 +0900

    2020.12.30

---
+++ src/step05_array01/Q2562/Answer1.java
@@ -0,0 +1,26 @@
+package step05_array01.Q2562;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Answer1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int max=0;
+		int a = 0;
+		for (int i=0; i<9; i++) {
+			int number = Integer.parseInt(br.readLine()); 
+			if (max < number) {
+				max = number;
+				a = i+1;
+			}
+			
+		}
+		System.out.println(max);
+		System.out.println(a);
+	}
+
+}
