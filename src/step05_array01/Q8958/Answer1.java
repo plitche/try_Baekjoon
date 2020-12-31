ece8691fb695aa1935a89c4675b522af3dbd7112
plitche <kysu728@naver.com>
2020-12-31 19:23:09 +0900

    2020.12.31

---
+++ src/step05_array01/Q8958/Answer1.java
@@ -0,0 +1,40 @@
+package step05_array01.Q8958;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Answer1 {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int testCase = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<testCase; i++) {
+            String ox = br.readLine();
+            int[] score = new int[ox.length()];
+            int result = 0;
+
+            if (ox.charAt(0)=='O') {
+                score[0] = 1;
+                result = 1;
+            }
+            for (int j=1; j<ox.length(); j++) {
+                if (ox.charAt(j)=='O') {
+                    score[j] = score[j-1]+1;
+                    result+=score[j];
+                }
+            }
+            bw.append(String.valueOf(result));
+            bw.newLine();
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+
+}
