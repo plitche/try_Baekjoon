b216e14df992634870e460c1f560af33636fa8cc
plitche <kysu728@naver.com>
2021-01-01 14:10:22 +0900

    2021.01.01

--- src/step06_function/Q15596/Answer1.java
+++ src/step06_function/Q15596/Answer1.java
@@ -1,5 +1,31 @@
 package step06_function.Q15596;
 
-public class Answer1 {
+import java.util.Scanner;
 
+public class Answer1 {
+	long sum(int[] a) {
+        long ans = 0;
+        for (int i=0; i<a.length; i++) {
+        	ans += a[i];
+        }
+        
+        return ans;
+    }
+    
+	public static void main(String[] args) {
+
+		Answer1 t = new Answer1();
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		
+		for (int i=0; i<n; i++) {
+			a[i] = sc.nextInt();
+		}
+		
+		System.out.println(t.sum(a));
+		
+		sc.close();
+	}
 }
