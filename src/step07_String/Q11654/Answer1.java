cb28d0879aec53c2bfed0e83dfe57dcce658d2f4
plitche <kysu728@naver.com>
2021-01-02 13:56:14 +0900

    2020.01.02

---
+++ src/step07_String/Q11654/Answer1.java
@@ -0,0 +1,17 @@
+package step07_String.Q11654;
+
+import java.util.Scanner;
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		
+		System.out.println((int)a.charAt(0));
+		
+	}
+
+}
+
---
+++ src/step07_String/Q11654/Answer2.java
@@ -0,0 +1,5 @@
+package step07_String.Q11654;
+
+public class Answer2 {
+
+}
