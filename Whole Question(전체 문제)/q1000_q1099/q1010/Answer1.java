0c4bcfafd68b634a4083f4e82df88725b923e5fa
plitche <kysu728@naver.com>
2021-01-08 01:38:33 +0900

    Test format change
    Test at Q1010
    Add format about Question URL & What i use to solve

---
+++ Whole Question(전체 문제)/q1000_q1099/q1010/Answer1.java
@@ -0,0 +1,29 @@
+/* 
+	작성자: 권용수
+	이메일: kysu728@naver.com / kysu728@gmail.com
+	주소: 서울시 마포구 노고산동
+	GitHub: https://github.com/plitche/try_Baekjoon
+*/
+
+// https://www.acmicpc.net/problem/1010
+
+// Scanner version(버전) 
+package q1000_q1099.q1010;
+
+import java.util.Scanner;
+
+public class Answer1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		for (int i=0; i<testCase; i++) {	//testCase만큼 입력 받기
+			
+		}
+		
+		
+	}
+	
+}
