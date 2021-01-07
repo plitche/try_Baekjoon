4297494d759a4103d59c12267e952c64c1662332
plitche <kysu728@naver.com>
2021-01-07 20:48:18 +0900

    Test Commit

--- By Step_단계별로 풀어보기/step09_math2/Q1929/Answer1.java
+++ By Step_단계별로 풀어보기/step09_math2/Q1929/Answer1.java
@@ -1,7 +1,7 @@
 /* 
 	작성자: 권용수
-	이메일: kysu728@naver.com
-	주소: 서울시 마포구
+	이메일: kysu728@naver.com / kysu728@gmail.com
+	주소: 서울시 마포구 노고산동
 	GitHub: https://github.com/plitche/try_Baekjoon
 */
 
--- Whole Question(전체 문제)/q1000_q1099/q1001q/Answer1.java
+++
@@ -1,5 +0,0 @@
-package q1000_q1099.q1001q;
-
-public class Answer1 {
-
-}
