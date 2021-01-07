7dd8b968451ac375b9531f399315fac7b1f4f684
plitche <kysu728@naver.com>
2021-01-08 02:06:02 +0900

    Save format
    Can't solved this question. But just to save format

--- Whole Question(전체 문제)/q1000_q1099/q1010/Answer1.java
+++ Whole Question(전체 문제)/q1000_q1099/q1010/Answer1.java
@@ -1,13 +1,5 @@
-/* 
-	작성자: 권용수
-	이메일: kysu728@naver.com / kysu728@gmail.com
-	주소: 서울시 마포구 노고산동
-	GitHub: https://github.com/plitche/try_Baekjoon
-*/
-
 // https://www.acmicpc.net/problem/1010
-
-// Scanner version(버전) 
+// Scanner version(버전)
 package q1000_q1099.q1010;
 
 import java.util.Scanner;
@@ -20,8 +12,13 @@ public class Answer1 {
 		
 		int testCase = sc.nextInt();
 		for (int i=0; i<testCase; i++) {	//testCase만큼 입력 받기
+			int west = sc.nextInt();	//서쪽 site 개수
+			int east = sc.nextInt();	//동쪽 site 개수
+			
+			
 			
 		}
+		
 		
 		
 	}
