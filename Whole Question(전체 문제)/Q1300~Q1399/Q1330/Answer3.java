67df2e1aec88459ac16edd3dbb496683f3a886f5
plitche <kysu728@naver.com>
2021-01-17 15:47:06 +0900

    Add solution Question
    Add solutions Q1316, Q1330's Answer1, Answer2, Answer3

---
+++ Whole Question(전체 문제)/Q1300~Q1399/Q1330/Answer3.java
@@ -0,0 +1,25 @@
+package step02_if.Q1330;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Answer3 {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        if (A>B) {
+            System.out.println('>');
+        } else if (A<B) {
+            System.out.println('<');
+        } else {
+            System.out.println("==");
+        }
+
+    }
+
+}
