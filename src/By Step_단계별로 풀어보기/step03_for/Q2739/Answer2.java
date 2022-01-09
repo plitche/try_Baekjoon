437ce01b50f2c957b723c9d5826fb1929587545e
plitche <kysu728@naver.com>
2022-01-09 18:07:54 +0900

    edit directory

---
+++ src/By Step_단계별로 풀어보기/step03_for/Q2739/Answer2.java
@@ -0,0 +1,23 @@
+package step03_for.Q2739;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Answer2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int dan = Integer.parseInt(br.readLine());
+	
+		for (int i=1; i<=9; i++) {
+			System.out.print(dan);
+			System.out.print(" * ");
+			System.out.print(i);
+			System.out.print(" = ");
+			System.out.println(dan*i);
+		}
+		
+	}
+	
+}
---
+++ src/By Step_단계별로 풀어보기/step03_for/Q2741/Answer1.java
@@ -0,0 +1,26 @@
+package step03_for.Q2741;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Answer1 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int i =0;
+		while (i++ < N) {
+			bw.append(String.valueOf(i));
+			bw.newLine();
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+	
+}
