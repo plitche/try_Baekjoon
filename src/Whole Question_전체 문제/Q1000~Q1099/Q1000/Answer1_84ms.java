f82b75136ad21afca0c8018fa2b6ebb2555ff235
plitche <kysu728@naver.com>
2022-02-08 00:00:36 +0900

    edit directory

---
+++ src/Whole Question_전체 문제/Q1000~Q1099/Q1000/Answer1_84ms.java
@@ -0,0 +1,28 @@
+/* 
+	작성자: 권용수
+	이메일: kysu728@naver.com / kysu728@gmail.com
+	주소: 서울시 마포구 노고산동
+	GitHub: https://github.com/plitche/try_Baekjoon
+*/
+
+package q1000_q1099.q1000;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Answer1_84ms {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str," ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        System.out.println(A+B);
+
+    }
+
+}
---
+++ src/Whole Question_전체 문제/Q1000~Q1099/Q1008/Answer1_88ms.java
@@ -0,0 +1,28 @@
+/* 
+	작성자: 권용수
+	이메일: kysu728@naver.com / kysu728@gmail.com
+	주소: 서울시 마포구 노고산동
+	GitHub: https://github.com/plitche/try_Baekjoon
+*/
+
+package q1000_q1099.q1008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Answer1_88ms {
+
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Double A = Double.parseDouble(st.nextToken());
+        Double B = Double.parseDouble(st.nextToken());
+
+        System.out.println(A/B);
+        br.close();
+	}
+
+}
