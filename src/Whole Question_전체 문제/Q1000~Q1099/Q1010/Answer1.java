c15f09c5a0db90dd9cecdcbf1d29d04870c41f05
plitche <kysu728@naver.com>
2022-02-09 00:51:32 +0900

    edit directory

---
+++ src/Whole Question_전체 문제/Q1000~Q1099/Q1010/Answer1.java
@@ -0,0 +1,26 @@
+// https://www.acmicpc.net/problem/1010
+// Scanner version(버전)
+package q1000_q1099.q1010;
+
+import java.util.Scanner;
+
+public class Answer1 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int testCase = sc.nextInt();
+		for (int i=0; i<testCase; i++) {	//testCase만큼 입력 받기
+			int west = sc.nextInt();	//서쪽 site 개수
+			int east = sc.nextInt();	//동쪽 site 개수
+			
+			
+			
+		}
+		
+		
+		
+	}
+	
+}
