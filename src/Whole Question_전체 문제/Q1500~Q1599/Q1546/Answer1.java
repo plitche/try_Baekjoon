da96249b24eeed275a15b202882b394eef1877a6
plitche <kysu728@naver.com>
2022-02-12 21:38:18 +0900

    edit directory

---
+++ src/Whole Question_전체 문제/Q1500~Q1599/Q1546/Answer1.java
@@ -0,0 +1,32 @@
+package step05_oneDimensionalArray.Q1546;
+
+import java.util.Scanner;
+
+public class Answer1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		int[] subject;
+		subject = new int[sc.nextInt()];
+		
+		int topScore=0;
+		for (int i=0; i<subject.length; i++) {
+			subject[i] = sc.nextInt();
+			if(topScore < subject[i]) {
+				topScore = subject[i];
+			}
+		}
+		
+		double sum=0;
+		double[] subject2 = new double[subject.length];
+		
+		for (int i=0; i<subject.length; i++) {
+			subject2[i] = (double)subject[i]/topScore*100;
+			sum += subject2[i];
+		}
+		System.out.println((double)sum/subject.length);
+	}
+
+}
